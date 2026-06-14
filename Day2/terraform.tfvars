0e34c366f009f10735aa2a524a874c995907d714
Santhoshkumar <santhosh5375kumar@gmail.com>
2026-06-14 19:00:55 +0530

    Updated Day2

---
+++ Day2/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id        = "ami-0f58b397bc5c1f2e8"
+instance_type = "t3.micro"
--- Day2/variable_implementation.tf
+++ Day2/variable_implementation.tf
@@ -1,37 +1,31 @@
-# variable Demo
+# Variable Demo
 
-'''hcl
-# Define an input variable for the EC2 instance Type
-
-variable "EC2 Instance type"{
-    description = "EC2 Instance type"
-    type = string
-    default = "t2,micro"
+# Define an input variable for the EC2 instance type
+variable "instance_type" {
+  description = "EC2 Instance type"
+  type        = string
+  default     = "t3.micro"
 }
 
 # Define an input variable for the EC2 instance AMI ID
-
-varible "ami-id"{
-    description = "EC2 ami ID"
-    type = string
-    
+variable "ami_id" {
+  description = "EC2 AMI ID"
+  type        = string
 }
 
-# Configure the AWS provider using the input variables
+# Configure the AWS provider
 provider "aws" {
-  region      = "ap-south-1"
+  region = "ap-south-1"
 }
 
-
-# Configure the AWS provider using the input variables
-
-resource "aws_instance" "example_instance"{
-    ami = var.ami_id
-    instance_type = var.instance_type
+# Create an EC2 instance using the input variables
+resource "aws_instance" "example_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
 }
 
-# Define an output variable to expose the public IP address of the EC2 instance
-output "public_ip{
-    description = "Public IP address of the EC2 instance"
-    value = aws_instance.example_instance.public_ip
+# Define an output variable to expose the public IP address
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.example_instance.public_ip
 }
